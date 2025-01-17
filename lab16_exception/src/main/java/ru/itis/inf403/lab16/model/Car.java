8c5efcee74f2032249bd1f4f66ff88c5e916700b
Roman <bazarnoffroman@yandex.ru>
2025-01-17 22:32:10 +0300

    lab16_exception

---
+++ lab16_exception/src/main/java/ru/itis/inf403/lab16/model/Car.java
@@ -0,0 +1,21 @@
+package ru.itis.inf403.lab16.model;
+import java.util.*;
+
+
+public class Car {
+
+    public boolean isBroken = false;
+    public int crew = 2;
+    public int seat = 5;
+
+
+    public void check(double probability) throws Exception{
+        if (new Random().nextInt(100) <= probability){
+            isBroken = true;
+            throw new Exception("Автомобиль сломался");
+        }
+    }
+
+
+
+}
