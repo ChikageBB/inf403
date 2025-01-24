ace2fe0751d84711f8c5538bb130af78531703ff
Roman <bazarnoffroman@yandex.ru>
2025-01-24 22:23:04 +0300

    changed the CalculateIntegralMiddle

--- lab18/src/main/java/ru/itis/inf403/lab18/model/CalculateIntegralMiddle.java
+++ lab18/src/main/java/ru/itis/inf403/lab18/model/CalculateIntegralMiddle.java
@@ -2,10 +2,21 @@ package ru.itis.inf403.lab18.model;
 
 public class CalculateIntegralMiddle extends CalculateIntegral{
     public CalculateIntegralMiddle(double a, double b, Function f){ super(a, b, f);}
+    public static final int H = 1000;
 
 
     public double get(){
-        return (b - a) * function.calculate((b - a) / 2);
+        double s = 0;
+
+        double h = (b - a) / H;
+
+        for (int i = 0; i < H; ++i){
+            s += h * function.calculate(a + h * (i + 0.5));
+        }
+
+
+//        return (b - a) * function.calculate((b - a) / 2);
+        return s;
     }
 
 }
